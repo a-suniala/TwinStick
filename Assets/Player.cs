3d74b95ba858d52494cfd168ef84ccf80eeb692c
Sunave <antti.suniala@gmail.com>
2015-08-31 16:16:50 +0300

    Initial commit

---
+++ Assets/Player.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+		Debug.Log ("H: " + CrossPlatformInputManager.GetAxis("Horizontal"));
+		Debug.Log ("V: " + CrossPlatformInputManager.GetAxis("Vertical"));
+	}
+}
